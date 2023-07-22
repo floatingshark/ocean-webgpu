937776c456e609c45073f074f9dfcc2eeb0a074f
samenachan <x4samezame@gmail.com>
2023-07-22 18:26:28 +0900

    write document comments into canvas class

--- source/ts/canvas.ts
+++ source/ts/canvas.ts
@@ -7,13 +7,15 @@ import * as glm from 'gl-matrix';
  * Canvas element class for webgl
  */
 export class Canvas {
+  /**
+   * @constructor
+   * @param {string} canvasID id of canvas element
+   */
   constructor(canvasId: string) {
     this.canvas = document.getElementById(canvasId) as HTMLCanvasElement;
     this.glContext = this.canvas.getContext('webgl2');
 
-    if (!this.initializeEventListener()) {
-      console.log('[CONSTRUCTOR]CANVAS INITIALIZE ERROR');
-    }
+    this.initializeEventListener();
 
     this.initializeShader();
     this.initializeAttribute();
@@ -23,70 +25,101 @@ export class Canvas {
     this.draw();
   }
 
+  /** canvas dom element */
   protected canvas: HTMLCanvasElement | null = null;
-  protected width: number = 512;
-  protected height: number = 512;
+  /** canvas element size */
+  protected size: number[] = [512, 512];
 
-  protected animTime: number = 0;
+  /** mouse cursor position on canvas */
   protected cursor: number[] = [0.0, 0.0];
+  /** mouse down or not */
   protected cursorOn: boolean = false;
 
+  /** webgl2.0 context this canvas */
   protected glContext: WebGLRenderingContext | null = null;
+  /** current shader program */
   protected program: WebGLProgram | null = null;
 
+  /** attribute buffer of vertices */
   protected vertexBuffer: WebGLBuffer | null = null;
+  /** attribute buffer of vertex colors */
   protected colorBuffer: WebGLBuffer | null = null;
+  /** index element buffer */
   protected indexBuffer: WebGLBuffer | null = null;
 
+  /** attribute location of vertices */
   protected vertexAttribLocation: number = 0;
+  /** attribute location of vertix colors */
   protected colorAttribLocation: number = 0;
 
+  /** uniform location of model matrix */
   protected uniformLocationModelMatrix: WebGLUniformLocation | null = null;
+  /** uniform location of view matrix */
   protected uniformLocationViewMatrix: WebGLUniformLocation | null = null;
+  /** uniform location of projection matrix */
   protected uniformLocationProjectionMatrix: WebGLUniformLocation | null = null;
 
+  /** model position */
   protected position: glm.vec3 = [0.0, 0.0, -1.0];
+  /** model rotation */
   protected rotation: glm.vec3 = [0.0, 0.0, 0.0];
+  /** model scale */
   protected scale: glm.vec3 = [1.0, 1.0, 1.0];
+  /** 4x4 model matrix */
   protected modelMatrix: glm.mat4 = glm.mat4.create();
 
+  /** view position a.k.a camera position */
   protected viewPosition: glm.vec3 = [2.0, 2.0, 5.0];
+  /** the position camera look at */
   protected viewLookAt: glm.vec3 = [0.0, 0.0, 0.0];
+  /** basis upvector , basically [0, 1, 0] or [0, 0, 0] and it depends on industry */
   protected viewUp: glm.vec3 = [0.0, 0.0, 1.0];
+  /** 4x4 view matrix */
   protected viewMatrix: glm.mat4 = glm.mat4.create();
 
+  /** camera FOV - field of range */
   protected projectionFovy: number = glm.glMatrix.toRadian(60.0);
-  protected projectionAspect: number = this.width / this.height;
+  /** camera aspect ratio - (width / height) */
+  protected projectionAspect: number = this.size[0] / this.size[1];
+  /** near bounds of camera */
   protected projectionNear: number = 0;
+  /** far bounds of camera */
   protected projectionFar: number = 10000;
+  /** 4x4 projection matrix */
   protected projectionMatrix: glm.mat4 = glm.mat4.create();
 
-  public getCanvas(): HTMLCanvasElement | null {
-    return this.canvas;
-  }
-  public getWidth(): number {
-    return this.width;
-  }
-  public setWidth(width: number) {
-    this.width = width;
-  }
-  public getHeight(): number {
-    return this.height;
-  }
-  public setHeight(height: number) {
-    this.height = height;
+  /**
+   * @returns {number[]} current canvas size
+   */
+  public getSize(): number[] {
+    return this.size;
   }
 
+  /**
+   * edit canvas size
+   * @param {number[]} size new size
+   */
+  public setSize(size: number[]) {
+    if (size.length > 0) {
+      this.size[0] = size[0];
+      this.size[1] = size[1];
+    }
+  }
+
+  /**
+   * Setup event listener
+   * @returns {boolean} setup is success or not
+   */
   protected initializeEventListener(): boolean {
     if (this.canvas === null) {
       return false;
     }
+
     this.canvas.addEventListener('mousemove', (e: MouseEvent) => {
       this.cursor[0] = e.offsetX;
       this.cursor[1] = e.offsetY;
       if (this.cursorOn) {
         this.viewPosition[0] += e.movementX;
-        console.log(this.viewPosition);
       }
     });
 
@@ -101,6 +134,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Setup shader program
+   * @returns {boolean} setup is success or not
+   */
   protected initializeShader(): boolean {
     if (this.canvas === null) {
       return false;
@@ -133,6 +170,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Setup shader attribute variables
+   * @returns {boolean} setup is success or not
+   */
   protected initializeAttribute(): boolean {
     if (this.glContext === null) {
       return false;
@@ -167,6 +208,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Register shader values to attribute buffers
+   * @returns {boolean} setup is success or not
+   */
   protected registerAttribute(): boolean {
     if (this.glContext === null) {
       return false;
@@ -188,6 +233,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Setup shader uniform variables
+   * @returns {boolean} setup is success or not
+   */
   protected initializeUniformLocation(): boolean {
     if (!this.glContext) {
       return false;
@@ -211,6 +260,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Register shader values to uniform variables
+   * @returns {boolean} setup is success or not
+   */
   protected registerUniform(): boolean {
     if (!this.glContext) {
       return false;
@@ -232,6 +285,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Rendering current shader context
+   * @returns {boolean} success or not
+   */
   protected draw(): boolean {
     if (this.glContext === null) {
       return false;
@@ -242,6 +299,10 @@ export class Canvas {
     return true;
   }
 
+  /**
+   * Evaluate this object working without problems
+   * @returns {boolean} working successfully or not
+   */
   public isValid(): boolean {
     let valid: boolean = true;
     valid &&= this.canvas !== null;
@@ -249,6 +310,9 @@ export class Canvas {
     return valid;
   }
 
+  /**
+   * Start canvas animation update loop
+   */
   public async beginAnimation() {
     function animationFramePromise(): Promise<number> {
       return new Promise<number>((resolve) => {
@@ -256,33 +320,45 @@ export class Canvas {
       });
     }
 
-    const startTime = Date.now();
     while (this.canvas) {
+      const prevTime = Date.now();
       await animationFramePromise();
-      this.animTime = Date.now() - startTime;
-      this.updateAnimation();
-      console.log(this.animTime);
+      const deltaTime = Date.now() - prevTime;
+      console.log(deltaTime);
+      this.updateAnimation(deltaTime);
     }
   }
 
-  public updateAnimation() {
+  /**
+   * Update loop function for canvas animation
+   * @param {number} deltaTime duration time between current and prev frame
+   */
+  protected updateAnimation(deltaTime: number) {
     if (this.canvas === null) {
       return;
     }
     if (this.glContext === null) {
       return;
     }
+    if (deltaTime <= 0.0) {
+      return;
+    }
 
     this.glContext.clearColor(0.0, 0.0, 0.0, 1.0);
     this.glContext.clear(this.glContext.COLOR_BUFFER_BIT);
 
-    this.calcurateMatrix();
+    this.calculateMvpMatrices();
+
     this.registerAttribute();
     this.registerUniform();
     this.draw();
   }
 
-  protected calcurateMatrix(): boolean {
+  /**
+   * Calc model, view, and projection matrices
+   * @returns {boolean} calculated successfully or not
+   */
+  protected calculateMvpMatrices(): boolean {
     // Create model matrix
     const translateMatrix: glm.mat4 = glm.mat4.translate(glm.mat4.create(), glm.mat4.create(), [
       this.position[0],
